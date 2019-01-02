11e1fa046f255c9a1b0556d323e9ebb5297938ce
Chris Roberson <chris.roberson@puppet.com>
2019-01-01 17:27:13 -0700

    Add RHEL/Centos Package.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,31 +1,42 @@
-# == Class: unifi::install
-
-class unifi::install {
+class unifi::install (
+  Boolean $manage_repository = true,
+){
   if $::unifi::manage_repository {
-    apt::key { 'unifi':
-      id     => $::unifi::repo_key_id,
-      server => $::unifi::repo_key_server,
-    }
+    case $facts['os']['family'] {
+      'redhat': {
+        yumrepo { 'unifi':
+          baseurl => 'http://dl.marmotte.net/rpms/redhat/el7/x86_64/unifi-controller-5.8.24-1.el7/',
+          descr   => 'repo',
+          enabled => '1',
+        }
+        package { 'unifi-controller-5.8.24-1.el7.x86_64.rpm':
+          ensure  => present,
+          require => Yumrepo['unifi'],
+        }
+      }
+      default: {
+        apt::key { 'unifi':
+          id     => $::unifi::repo_key_id,
+          server => $::unifi::repo_key_server,
+        }
 
-    apt::source { 'unifi':
-      comment  => 'Ubiquiti UniFi Controller APT Repository',
-      location => $::unifi::repo_location,
-      release  => $::unifi::repo_release,
-      require  => Apt::Key['unifi'],
-      repos    => 'ubiquiti',
-      pin      => '200',
-      include  => {
-        src => false,
-        deb => true,
-      },
-    }
-
-    Apt::Source['unifi'] -> Package[$::unifi::package_name]
-  }
-
-  if $::unifi::manage_package {
-    package { $::unifi::package_name:
-      ensure => $::unifi::package_ensure,
+        apt::source { 'unifi':
+          comment  => 'Ubiquiti UniFi Controller APT Repository',
+          location => $::unifi::repo_location,
+          release  => $::unifi::repo_release,
+          require  => Apt::Key['unifi'],
+          repos    => 'ubiquiti',
+          pin      => '200',
+          include  => {
+            src => false,
+            deb => true,
+          },
+        }
+        package { $::unifi::package_name:
+          ensure  => $::unifi::package_ensure,
+          require => Apt::Source['unifi'],
+        }
+      }
     }
   }
 }
